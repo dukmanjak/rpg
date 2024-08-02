c65dad6fa7d91df6ee8d8f0709f23b842b488f54
dukmanjak <dukmanjak@gmail.com>
2024-08-02 15:54:46 -0700

    Tutorial Innitial
    On lesson 12 or 13

---
+++ Dungeon RPG/Scripts/Player/Player.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody3D
+{
+    public override void _PhysicsProcess(double delta)
+    {
+        
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        GD.Print("Player Input");
+    }
+
+}
