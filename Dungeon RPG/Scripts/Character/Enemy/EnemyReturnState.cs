eb3decb08b0257b89432308e047fa49683d7376c
Steven Hanson II <dukmanjak@gmail.com>
2024-08-25 18:55:40 -0700

    2 lessons done

--- Dungeon RPG/Scripts/Character/Enemy/EnemyReturnState.cs
+++ Dungeon RPG/Scripts/Character/Enemy/EnemyReturnState.cs
@@ -3,8 +3,32 @@ using System;
 
 public partial class EnemyReturnState : EnemyState
 {
-    protected override void EnterState()
+    private Vector3 destination;
+ public override void _Ready()
+    {
+        base._Ready();
+
+        Vector3 localPos = characterNode.PathNode.Curve
+            .GetPointPosition(0);
+        Vector3 globalPos = characterNode.PathNode.GlobalPosition;
+        destination = localPos + globalPos;
+    }
+
+    
+       protected override void EnterState()
     {
         characterNode.AnimPlayerNode.Play(GameConstants.ANIM_MOVE);
     }
+    public override void _PhysicsProcess(double delta){
+
+        if(characterNode.GlobalPosition == destination){
+            GD.Print("Reached destination!");
+            return;
+        }
+
+        characterNode.Velocity = characterNode.GlobalPosition.DirectionTo(destination);
+
+        characterNode.MoveAndSlide();
+    }
+
 }
